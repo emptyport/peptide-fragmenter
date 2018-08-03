a00d2b0bfe54718ecf1f5d1042267555e5da5b24
Michael Porter <michaelporter.chemistry@gmail.com>
2018-08-03 16:25:06 -0500

    things seem to be working now. just need to write tests

--- example.js
+++ example.js
@@ -1,14 +1,3 @@
-var peptide_cutter = require('./index');
+var pepFrag = require('./index');
 
-var cut = new peptide_cutter({
-    'enzyme': 'trypsin',
-    'num_missed_cleavages': 0,
-    'min_length': 2,
-    'max_length': 30
-});
-console.log(cut);
-
-var peptides = cut.cleave("MRPAAAWKPAAAMKPAAAWRPAAAKIIIRPNNN");
-for(var i=0; i<peptides.length; i++) {
-    console.log(peptides[i]);
-}
+console.log(pepFrag.fragment('ELVISLIVES', ['a','b','c','x','y','z'], [1]));
--- index.js
+++ index.js
@@ -1,91 +1,117 @@
-var expasy_rules = require('./expasy_rules.json');
+const residue_masses = {
+  "A": 71.03711,
+  "R": 156.10111,
+  "N": 114.04293,
+  "D": 115.02694,
+  "C": 103.00919,
+  "E": 129.04259,
+  "Q": 128.05858,
+  "G": 57.02146,
+  "H": 137.05891,
+  "I": 113.08406,
+  "L": 113.08406,
+  "K": 128.09496,
+  "M": 131.04049,
+  "F": 147.06841,
+  "P": 97.05276,
+  "S": 87.03203,
+  "T": 101.04768,
+  "W": 186.07931,
+  "Y": 163.06333,
+  "V": 99.06841
+};
 
-module.exports = class PeptideCutter {
-  constructor(options = {}) {
-    // These are the default values. They will
-    // be overwritten by anything passed in 
-    // through the options
-    this.enzyme = 'trypsin';
-    this.num_missed_cleavages = 0;
-    this.min_length = 8;
-    this.max_length = 30;
+// These are the mass shifts to go from the residue mass to the ion
+// This is a helpful resource https://www.uab.edu/proteomics/pdf_files/2011/BMG744-01-14-11.pdf
+const A_SHIFT = -27.994915; // -CO
+const B_SHIFT = 0; // Nothing
+const C_SHIFT = 17.026548; // NH3
+const X_SHIFT = 27.994915 + 18.010565 - (2 * 1.007276466812); // CO2
+const Y_SHIFT = 18.010565; // H2O
+const Z_SHIFT = -17.026548 + 18.010565; // -NH3 + H2O
 
-    if(options.enzyme !== undefined) {
-      this.enzyme = options.enzyme;
-      if(expasy_rules[this.enzyme] === undefined) {
-        console.log("Invalid enzyme. Defaulting to trypsin");
-        this.enzyme = 'trypsin';
-      }
-    }
-    this.regex = new RegExp(expasy_rules[this.enzyme], "g");
+const PROTON = 1.007276466812;
 
-    if(options.num_missed_cleavages !== undefined) {
-      this.num_missed_cleavages = options.num_missed_cleavages;
-      if(this.num_missed_cleavages < 0) {
-        console.log("The number of missed cleavages can't be less than 0. Defaulting to 0");
-        this.num_missed_cleavages = 0;
-      }
-    }
+module.exports.fragment = function(sequence, ion_types=['b', 'y'], fragment_charges=[1],modifications={}) {
+  sequence = sequence.toUpperCase();
+  const ILLEGAL = [ 'B', 'J', 'O', 'U', 'X', 'Z' ];
 
-    if(options.min_length !== undefined) {
-      this.min_length = options.min_length;
-    }
-    if(options.max_length !== undefined) {
-      this.max_length = options.max_length;
-    }
-    if(this.min_length > this.max_length) {
-      console.log("The min_length cannot be greater than the max_length. Defaulting to 8 and 30");
-      this.min_length = 8;
-      this.max_length = 30;
-    }
+  let sequenceList = sequence.split("");
+
+  // For now we don't handle ambiguous symbols, just return nothing
+  let overlap = sequenceList.filter(val => -1 !== ILLEGAL.indexOf(val));
+  if(overlap.length > 0) {
+    return {};
   }
 
-  uniqueFilter(value, index, self) {
-    return self.indexOf(value) === index;
+  // Convert the residues to masses
+  let massList = sequenceList.map((residue) => {
+    return residue_masses[residue];
+  });
+
+  // Add in the modifications
+  for (var mod in modifications) {
+    let pos = mod.position;
+    let mass = mod.mass;
+    massList[pos] = massList[pos] + mass;
   }
 
-  cleave(sequence) {
-    sequence = sequence.toUpperCase();
-    var peptides = [];
-    var num_missed = this.num_missed_cleavages;
-
-    // Gather the cleavage sites
-    var indices = [0];
-    var m;
-    do {
-      m = this.regex.exec(sequence);
-      if (m) {
-        indices.push(m.index + 1);
-      }
-    } while (m);
-    indices.push(sequence.length);
-
-    // If there are only 3 cleavage sites there
-    // is no reason to allow more than 3 missed
-    // cleavages
-    if(indices.length < num_missed) {
-      num_missed = indices.length;
-    }
-
-    for(var i=0; i<indices.length; i++) {
-      for(var j=i; j<indices.length; j++) {
-        if(j-i-1 > num_missed) {
-          break;
-        }
-        var start_index = indices[i];
-        var end_index = indices[j];
-        if(start_index === end_index) {
-          continue;
-        }
-        if(end_index - start_index >= this.min_length && end_index - start_index <= this.max_length) {
-          peptides.push(sequence.substring(start_index, end_index));
-        }
-      }
-    }
-
-    peptides = peptides.filter(this.uniqueFilter);
-
-    return peptides;
+  // Accumulate the masses into a running total both forwards and backwards
+  let len = massList.length;
+  let forwardMasses = new Array(len);
+  let backwardMasses = new Array(len);
+  forwardMasses[0] = massList[0];
+  backwardMasses[len-1] = massList[len-1];
+  for(var i=1; i<massList.length; i++) {
+    let j = len - 1 - i;
+    forwardMasses[i] = forwardMasses[i-1] + massList[i];
+    backwardMasses[j] = backwardMasses[j+1] + massList[j];
   }
+  forwardMasses.pop();
+  backwardMasses.shift();
 
+  let results = {};
+
+  // Now we go over the ion types and fragment charges
+  ion_types.forEach(function(type) {
+    let ions = [];
+    results[type] = {};
+    switch(type) {
+      case 'a':
+        ions = calculateIons(A_SHIFT, forwardMasses);
+        break;
+      case 'b':
+        ions = calculateIons(B_SHIFT, forwardMasses);
+        break;
+      case 'c':
+        ions = calculateIons(C_SHIFT, forwardMasses);
+        break;
+      case 'x':
+        ions = calculateIons(X_SHIFT, backwardMasses);
+        break;
+      case 'y':
+        ions = calculateIons(Y_SHIFT, backwardMasses);
+        break;
+      case 'z':
+        ions = calculateIons(Z_SHIFT, backwardMasses);
+        break;
+      default:
+        break;
+    }
+    fragment_charges.forEach(function(charge) {
+      let charged_ions = ions.map((ion) => {
+        return (ion + charge * PROTON) / charge;
+      });
+      results[type][charge] = charged_ions;
+    })
+  });
+  
+  return results;
+}
+
+calculateIons = (shift, masses) => {
+  let ions = masses.map((mass) => {
+    return mass + shift;
+  });
+  return ions;
 }
